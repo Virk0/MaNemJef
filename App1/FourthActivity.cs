11a35a77750bef0162ed667ca3d59fde8106431a
Virk0 <virko.veelmaa@tptlive.ee>
2018-09-10 08:45:15 +0300

    test
    okay

---
+++ App1/FourthActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    [Activity(Label = "FourthActivity")]
+    public class FourthActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.fourth_layout);
+            var textview1 = FindViewById<TextView>(Resource.Id.textView1);
+
+            var myText = Intent.GetStringExtra("MyData");
+            textview1.Text = myText;
+        }
+    }
+}
---
+++ App1/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            int count = 0;
+            var textView = FindViewById<TextView>(Resource.Id.textView1);
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var buttonSecondActivity = FindViewById<Button>(Resource.Id.button2);
+            var buttonThirdActivity = FindViewById<Button>(Resource.Id.button3);
+            var buttonFourthActivity = FindViewById<Button>(Resource.Id.button4);
+
+            button.Click += delegate
+            {
+                count++;
+                textView.Text = "OMEGALUL'd " + count + " times";
+            };
+
+            buttonSecondActivity.Click += ButtonSecondActivity_Click;
+            buttonThirdActivity.Click += ButtonThirdActivity_Click;
+            buttonFourthActivity.Click += ButtonFourthActivity_Click;
+        }
+
+        private void ButtonFourthActivity_Click(object sender, System.EventArgs e)
+        {
+            var fourthActivity = new Intent(this, typeof(FourthActivity));
+
+            fourthActivity.PutExtra("MyData", "Moi data");
+            StartActivity(fourthActivity);
+        }
+
+        private void ButtonThirdActivity_Click(object sender, System.EventArgs e)
+        {
+            var thirdActivity = new Intent(this, typeof(ThirdActivity));
+            StartActivity(thirdActivity);
+        }
+
+        private void ButtonSecondActivity_Click(object sender, System.EventArgs e)
+        {
+            var secondActivity = new Intent(this, typeof(SecondActivity));
+            StartActivity(secondActivity);
+        }
+
+    }
+}
